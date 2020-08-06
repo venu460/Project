5709931accb6fb20f54b32760ab6fde0f23ded72
venu.vellampalli@yash.com <venu.vellampalli@yash.com>
2020-08-06 18:44:37 +0530

    ggg

---
+++ com.Project1/src/test/java/com/Project1/Firstone.java
@@ -0,0 +1,17 @@
+package com.Project1;
+
+import org.testng.annotations.Test;
+
+public class Firstone {
+	
+	@Test
+	void abc()
+	{
+		System.out.println("Hi");
+		
+		System.out.println("Hii");
+		
+		System.out.println("Hiii");
+	}
+
+}
